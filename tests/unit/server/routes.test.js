c3f4e4bb41ee0439456e3e4070a6ff36b9c54cca
PabloMelo <pablomelodev@gmail.com>
2022-03-19 18:04:54 -0300

    feat: create util test and create first test

--- tests/unit/server/routes.test.js
+++ tests/unit/server/routes.test.js
@@ -1,7 +1,46 @@
-import { jest, describe, test, expect } from '@jest/globals';
+import { jest, describe, test, expect, beforeEach } from '@jest/globals';
+
+import { handler } from '../../../server/routes.js';
+import config from '../../../server/config.js';
+import TestUtil from '../_util/testUtil.js';
 
 describe('Suite Routes', () => {
-  test('sum two number', () => {
-    expect(2 + 2).toBe(4);
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.clearAllMocks();
+  });
+
+  test('GET / - should redirect to home page', async () => {
+    const params = TestUtil.defaultHandleParams();
+
+    params.request.method = 'GET';
+    params.request.url = '/';
+
+    await handler(...params.values());
+
+    expect(params.response.end).toHaveBeenCalled();
+
+    expect(params.response.writeHead).toBeCalledWith(302, {
+      Location: config.location.home,
+    });
+  });
+
+  test.todo(
+    `GET /home - should response with ${config.pages.homeHTML} file stream`
+  );
+
+  test.todo(
+    `GET /controller - should response ${config.pages.controllerHTML} file stream`
+  );
+
+  test.todo('GET /file.ext - should response with file stream');
+
+  test.todo(
+    'GET /unknown - given an inexistent route it should response with 404'
+  );
+
+  describe('exceptions', () => {
+    test.todo('given inexistent file it should respond with 404');
+    test.todo('given an error it should respond with 500');
   });
 });
